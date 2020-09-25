37e08b633de783e322078a62b90eac3aa2a10495
JamisonBlair <42307515+JamisonBlair@users.noreply.github.com>
2020-09-24 23:54:40 -0700

    Update upDownGame.py

--- upDownGame.py
+++ upDownGame.py
@@ -9,26 +9,34 @@ import upDown as uD
 import matplotlib.pyplot as plt
 
 cont = 'y'
+same_game = 'n'
 while cont == 'y':
     plt.close("all")
-    while True:
-        try:            
-            n = abs(int(input("Number of vertices: ")))
-            break
-        except ValueError:
-            print("Please choose an integer.")
-    while True:
-        c = input("Colors? [y/n]: ")
-        if c in {'y','n'}:
-            break
-        else:
-            print("Please choose a valid option.")
+    if same_game != 'y':
+        while True:
+            try:            
+                n = abs(int(input("Number of vertices: ")))
+                break
+            except ValueError:
+                print("Please choose an integer.")
+        while True:
+            c = input("Colors? [y/n]: ")
+            if c == 'upsetupsetdownsetdownsetleftsetrightsetleftsetrightsetbsetasetstartset':
+                vs = input("Choose vertex style. [ball/star]: ")
+                if vs == 'star':
+                    marker = "*"
+            if c in {'y','n'}:
+                break
+            else:
+                print("Please choose a valid option.")
     
     if c == 'y':
         colored = True
     else:
         colored = False
-    
-    G = uD.RandomUpDown(n, colored=colored)
-    G.play()
+    if same_game != 'y':
+    	G = uD.RandomUpDown(n, colored=colored)
+    G.play(marker=marker)
     cont = input("Play again? [y/n]: ")
+    if cont == 'y':
+        same_game = input("Same game? [y/n]: ")
