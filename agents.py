0ad3d66b22a9fe1a97ad97ee57a8abcc19d18531
charliePetersen <71359061+charliePetersen@users.noreply.github.com>
2021-01-18 11:16:39 -0800

    Add files via upload

--- agents.py
+++ agents.py
@@ -4,9 +4,58 @@
 @author: Charles Petersen and Jamison Barsotti
 """
 
+import model 
+import gameState as gs
+import mcts
+
 import random
+import torch
+import numpy as np
+
+def alpha_zero(game, player, search_iters=800):
+    '''Returns the deterministic move suggetsed by current best model after 
+    performing an MCTS with 'search_iters' iterations on 'game' from the
+    perspective of 'player'.
+
+    Parameters
+    ----------
+    game : UpDown
+        a game of upset-downset.
+    player : str
+        'Up' if the agent is to be the Up player, and 'Down' if the agent is
+        to be the Down player.
+    search_iters : int (nonegative), optional
+        teh number of MCTS iterations to perform. The default is 800.
+
+    Returns
+    -------
+    move : int (nonnegative)
+        node in 'game'.
+
+    '''
+    cur_player = gs.UP if player == 'Up' else gs.DOWN
+    #load agent
+    agent = model.Net(input_shape=model.OBS_SHAPE, actions_n=gs.UNIV)
+    agent.load_state_dict(torch.load('alpha_net.pt'))
+    
+    # encode game
+    state = gs.to_state(game, to_move=cur_player)
+    
+    # run mcts
+    root = mcts.PUCTNode(state)
+    mcts.MCTS(root, agent, num_iters=search_iters)
+    
+    # get move via deterministic mcts policy
+    actions = np.arange(gs.UNIV)
+    policy = mcts.MCTS_policy(root, temp=0)
+    move = np.random.choice(actions, p=policy)
+    
+    return move
+
+
 def random_agent(game, player):
     ''' Returns a random move in the upset-downset 'game' for 'player'.
+    
     Parameters
     ----------
     game : UpDown
@@ -22,107 +71,7 @@ def random_agent(game, player):
         the agent is to make its random play.
 
     '''
-    # Determine which colors agent chooses.
-    if player == 'Up':
-        colors = {0,1}
-    else:
-        colors = {-1,0}   
-    # Find the list of opions for the agent
-    options = list(filter(lambda x : game.coloring[x] in \
-                          colors, list(game.elements)))
-    # Return a random choice.    
+    # Determine which nodes can be played
+    options = game.up_nodes() if player == 'UP' else game.down_nodes()
+
     return random.choice(options)
-
-
-def min_agent(game, player):
-    ''' Returns a move in the upset-downset 'game' for 'player' which removes 
-    the least elements from the board.
-    
-    Parameters
-    ----------
-    game : UpDown
-        a game of upset-downset
-    player : str
-        'Up' if the agent is to be the Up player, and 'Down' if the agent is
-        to be the Down player.
-
-    Returns
-    -------
-    int
-        element in the poset underlying the upset-downset 'game' on which 
-        the agent is to make its minimum play.
-
-    '''
-    # Determine which colors agent chooses.
-    if player == 'Up':
-        colors = {0,1}
-    else:
-        colors = {-1,0}    
-    # Find the list of opions for the agent
-    options = list(filter(lambda x : game.coloring[x] in \
-                          colors, list(game.dag)))
-    # pick option which removes the least elements from the board
-    u = options[0]
-    if player == 'Up':
-        size = len(game.upset(u))
-        for x in options[1:]:
-            size_x = len(game.upset(x))
-            if size > size_x:
-                u = x
-                size = size_x
-    else:
-        size = len(game.downset(u))
-        for x in options[1:]:
-            size_x = len(game.downset(x))
-            if size > size_x:
-                u = x
-                size = size_x
-    return u
-
-def max_agent(game, player): 
-    ''' Returns a move in the upset-downset 'game' for 'player' which removes 
-    the most elements from the board.
-    
-    Parameters
-    ----------
-    game : UpDown
-        a game of upset-downset
-    player : str
-        'Up' if the agent is to be the Up player, and 'Down' if the agent is
-        to be the Down player.
-
-    Returns
-    -------
-    int
-        element in the poset underlying the upset-downset 'game' on which 
-        the agent is to make its maximum play.
-
-    '''
-    # Determine which colors agent chooses.
-    if player == 'Up':
-        colors = {0,1}
-    else:
-        colors = {-1,0} 
-    # Find the list of opions for the agent
-    options = list(filter(lambda x : game.coloring[x] in \
-                          colors, list(game.dag)))
-    # pick option which removes the most elements from the board
-    u = options[0]
-    if player == 'Up':
-        size = len(game.upset(u))
-        for x in options[1:]:
-            size_x = len(game.upset(x))
-            if size < size_x:
-                u = x
-                size = size_x
-    else:
-        size = len(game.downset(u))
-        for x in options[1:]:
-            size_x = len(game.downset(x))
-            if size < size_x:
-                u = x
-                size = size_x
-    return u
-       
-        
-    
